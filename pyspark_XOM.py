827e7c7397e0da6d848c9a400538dc03d9b57a27
adhiraammu <92908566+adhiraammu@users.noreply.github.com>
2024-05-01 18:02:54 -0400

    Create pyspark_XOM.py
    Creating Pyspark code for XOM

---
+++ pyspark_XOM.py
@@ -0,0 +1,26 @@
+#Exxon
+
+from google.cloud import storage
+import requests
+import os
+from datetime import datetime
+from flask import escape
+
+def fetch_and_store_stock_data(request):
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    # Initialize Cloud Storage client
+    storage_client = storage.Client()
+    bucket = storage_client.bucket('stock767_xom')
+
+    # Function to fetch stock market data
+    api_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=XOM&apikey=311DM75JHN4UE6UD"
+    response = requests.get(api_url)
+    data = response.text
+
+    filename = f"stock_data_{datetime.now().strftime('%Y%m%d%H%M%S')}.json"
+
+    blob = bucket.blob(filename)
+    blob.upload_from_string(data, content_type='application/json')
+    return f"File {filename} uploaded to {bucket.name}."
