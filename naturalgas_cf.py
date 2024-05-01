6c0a48f951f5c7975cedda15a07a08ec3dbe0221
Animesh Nandan <83339335+animeshnandan@users.noreply.github.com>
2024-04-30 23:31:11 -0400

    Update and rename natural_gas_main.py to naturalgas_cf.py

--- naturalgas_cf.py
+++ naturalgas_cf.py
@@ -1,28 +1,24 @@
+#Natural Gas
 from google.cloud import storage
 import requests
 import os
 from datetime import datetime
 from flask import escape
 
-def fetch_and_store_natural_gas_data(request):
+def fetch_and_store_stock_data(request):
     request_json = request.get_json(silent=True)
     request_args = request.args
-
-    # You can use request_json or request_args to customize behavior via the request.
-
-    # Set up authentication
-    # os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "a537c1d5b80146b121346edfca8523fbe9ff83ae"
 
     # Initialize Cloud Storage client
     storage_client = storage.Client()
     bucket = storage_client.bucket('gasus_767')
 
-    # Function to fetch natural data
+    # Function to fetch stock market data
     api_url = "https://www.alphavantage.co/query?function=NATURAL_GAS&interval=daily&apikey=I1YA2VVJANR8KKLJ"
     response = requests.get(api_url)
     data = response.text
 
-    filename = f"naturalgas_data_{datetime.now()}.json"
+    filename = f"naturalgas_data_{datetime.now().strftime('%Y%m%d%H%M%S')}.json"
 
     blob = bucket.blob(filename)
     blob.upload_from_string(data, content_type='application/json')
