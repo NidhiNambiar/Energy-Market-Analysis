bdedde09040a2d55ff3ab2096fb5393bf75a9b81
Mohamed Salama <146989288+Msalamaumd@users.noreply.github.com>
2024-05-13 15:28:22 -0400

    Update bp_stock.sql

--- bigqueries/consistent patterns of volume preceding or following price changes/bp_stock.sql
+++ bigqueries/consistent patterns of volume preceding or following price changes/bp_stock.sql
@@ -7,3 +7,8 @@ SELECT
   FROM
     `learned-vault-378620.stocks_767.bpstock` AS t1
     INNER JOIN `learned-vault-378620.stocks_767.bpstock` AS t2 ON t1.Date = t2.Date - 1
+
+SELECT 
+     t1.Date,
+     t1.Volume, 
+     t2.Close - t1.Close As price_change
